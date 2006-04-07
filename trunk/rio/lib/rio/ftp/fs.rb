de9a8335676d35c2a12ece6e772a0d87e41c224a
rio4ruby <rio4ruby@15e1cb93-1811-0410-b9d1-a500f5877eb0>
2006-04-07 21:07:38 +0000

    added ftp/fs.rb
    git-svn-id: svn://rubyforge.org/var/svn/rio@80 15e1cb93-1811-0410-b9d1-a500f5877eb0

---
+++ trunk/rio/lib/rio/ftp/fs.rb
@@ -0,0 +1,65 @@
+#--
+# =============================================================================== 
+# Copyright (c) 2005, 2006 Christopher Kleckner
+# All rights reserved
+#
+# This file is part of the Rio library for ruby.
+#
+# Rio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rio; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# =============================================================================== 
+#++
+#
+# To create the documentation for Rio run the command
+#  ruby build_doc.rb
+# from the distribution directory. Then point your browser at the 'doc/rdoc' directory.
+#
+# Suggested Reading
+# * RIO::Doc::SYNOPSIS
+# * RIO::Doc::INTRO
+# * RIO::Doc::HOWTO
+# * RIO::Rio
+#
+# <b>Rio is pre-alpha software. 
+# The documented interface and behavior is subject to change without notice.</b>
+
+require 'rio/fs/impl'
+require 'rio/fs/base'
+require 'rio/ftp/impl'
+
+module RIO
+  module FTP
+    class FS < RIO::FS::Base
+      attr_reader :file,:dir
+      def initialize(uri)
+        @test = RIO::FTP::Impl::Test.new(uri)
+        @file = ::File
+        @dir  = RIO::FTP::Impl::Dir.new(uri)
+        @path = nil
+        @util = nil
+      end
+
+      def self.create(*args)
+        new(*args)
+      end
+      include RIO::FS::Str
+      include RIO::FS::File
+      include RIO::FS::Test
+      include RIO::FS::Dir
+#      def symlink?(s) false end
+#      def directory?(s) false end
+
+    end
+  end
+end
